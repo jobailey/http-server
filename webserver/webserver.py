720b04be840b493fcc4064d2f72b34d49333ce35
jobailey <44002188+jobailey@users.noreply.github.com>
2019-02-03 23:32:54 -0500

    Include Pydoc comments

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,18 +1,25 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class Serve(BaseHTTPRequestHandler):
+# Making the class for webserver.py
+class Serv(BaseHTTPRequestHandler):
 
-  def do_GET(self):
-    if self.path == '/':
-      self.path = '/index.html'
-    try:
-        file_to_open = open(self.path[1:]).read()
-        self.send_response(200)
-    except: 
-        file_to_open = "File not found"
-        self.send_response(404)
-    self.end_headers()
-    self.wfile.write(bytes(file_to_open, 'utf-8'))
+    # Inherit methods from BaseHTTPRequestHandler
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        
+        # Read file to be accessed by the user 
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except: 
+            file_to_open = "File not found"
+            self.send_response(404)
+        self.end_headers()
 
-httpd = HTTPServer(('localhost', 8080), Serve)
+        # Write file contents to the screen
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+# HTTP Daemon runs in the background and deals with server requests
+httpd = HTTPServer(('localhost', 8080), Serv)
 httpd.serve_forever()
